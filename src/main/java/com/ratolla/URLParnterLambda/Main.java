a156289fa8c9005e521b3e8ea2caa2725c54399b
Vitor Soares <vitorratollasoares@gmail.com>
2024-11-19 18:35:12 -0300

    Conexão e Inserção de Dados na S3

--- src/main/java/com/ratolla/URLParnterLambda/Main.java
+++ src/main/java/com/ratolla/URLParnterLambda/Main.java
@@ -4,6 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,9 +14,13 @@ import java.util.UUID;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>>{
 
+    // Dependência responsável por transformar os dados JSON em um Map
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // Método responsável por cuidar das requisições HTTP
+    // Depedência de Cliente da S3
+    private final S3Client s3Client = S3Client.builder().build();
+
+    // Metodo responsável por acessar os dados do Body e por salvá-los no Bucket da S3
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
         // Capturar dados do Body da Requisição HTTP
@@ -26,15 +33,32 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
             throw new RuntimeException("error parsing JSON body: " + exception.getMessage());
         }
 
-        // Acessar dados
+        // Acessar dados do Map das chaves correspondentes
         String origjnalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get(("expirationTime"));
+        // Conversão de TimeStamp para segundos
+        long expirationTimeInSeconds = Long.parseLong(expirationTime);
 
-    String shortUrlCode = UUID.randomUUID().toString().substring(0,8);
+        // Criação de um UUID para identificar cada URL
+        String shortUrlCode = UUID.randomUUID().toString().substring(0,8);
 
-    Map<String,String> response = new HashMap();
-    response.put("code", shortUrlCode);
+        // Criação de Objeto de URL
+        UrlData urlData = new UrlData(origjnalUrl, expirationTimeInSeconds);
 
+        // Conexão com a S3 e Inserção dos dados no Bucket
+        try{
+            String urlDataJson = objectMapper.writeValueAsString(urlData);
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket("bucket-urlshortener-storage")
+                    .key(shortUrlCode + ".json")
+                    .build();
+            s3Client.putObject(request, RequestBody.fromString(urlDataJson));
+        } catch (Exception ex){
+            throw  new RuntimeException(("Erro ao salvar dado no bucket" + ex.getMessage()));
+        }
+
+        Map<String,String> response = new HashMap();
+        response.put("code", shortUrlCode);
 
         return response;
     }
